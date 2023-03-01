c9995eb7488e0b2a67999c6c20214ca41839b5c5
vishalsharma121 <vishal.sharma@starkedge.com>
2023-03-01 16:46:38 +0530

    React Router Setup + Usage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,40 +2,44 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
-  return (
-    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-    <div className="container">
-      <a className="navbar-brand" href="/">{props.title}</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Project</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">{props.aboutText}</a>
-          </li>
-        </ul>
-        <form className="d-flex">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-success" type="submit">Search</button>
-        </form>
+return (
+  <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+  <div className="container">
+    <a className="navbar-brand" href="/">{props.title}</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="d-flex w-100 j-spece-between navbar-conollapse align-items-center" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Project</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">{props.aboutText}</a>
+        </li>
+      </ul>
+      <form className="d-flex mx-5">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-success" type="submit">Search</button>
+      </form>
+      <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+      <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+      <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Darkmode</label>
       </div>
-    </div>
-    </nav>
-  )
+          </div>
+        </div>
+  </nav>
+)
 }
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    aboutText:PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  aboutText:PropTypes.string.isRequired
 }
 // Specifies the default values for props:
 Navbar.defaultProps = {
-  title: 'set title here',
-  aboutText:'About text here',
+title: 'set title here',
+aboutText:'About text here',
 };
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,16 +4,33 @@ export default function TextForm(props) {
     const handleUpClick = ()=>{
         console.log("Uppercase eas clicked "  +  text);
         let newText = text.toUpperCase();
-        setText(newText)
+        setText(newText);
+        props.showAlert("converted to uppercase", "success");
     }
     const handlelowClick = ()=>{
         console.log("Uppercase eas clicked "  +  text);
         let newText = text.toLowerCase();
-        setText(newText)
+        setText(newText);
+        props.showAlert("converted to lowercase", "success");
     }
     const handleOnChange = (event)=>{
-        console.log("On chnage");
+        console.log("on chnage");
         setText(event.target.value);
+    }
+    const handleclrClick = ()=>{
+        setText("");
+        props.showAlert("text clered", "success");
+    }
+    const handleCopy = ()=>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("text copy", "success");
+    }
+    const handleExtraSpaces = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("removed extra speces", "success");
     }
 
     const [text, setText] = useState('Enter text here');
@@ -21,26 +38,29 @@ export default function TextForm(props) {
     // setText("new text"); //correct way to change the state
 return (
 <>
-<div className="container mt-3">
+<div className="container mt-3" style={{color:props.mode==='dark'?'white':'black',}}>
     <h1>{ props.formHeading }</h1>
 <div className="mb-3">
-<input type="email" className="form-control" id="mail" placeholder="name@example.com" />
+<input type="email" className="form-control" style={{backgroundColor:props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'white':'black'}} id="mail" placeholder="" />
 </div>
 <div className="mb-3">
 <label htmlFor="text" className="form-label">Example textarea</label>
-<textarea className="form-control" placeholder={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+<textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'white':'black'}}  id="myBox" rows="8"></textarea>
 </div>
 <div className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upercase</div>
-<div className="btn btn-primary max-2" onClick={handlelowClick}>Convert to Lowercase</div>
+<div className="btn btn-primary mx-2" onClick={handlelowClick}>Convert to Lowercase</div>
+<div className="btn btn-primary mx-2" onClick={handleclrClick}>Clear All</div>
+<div className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</div>
+<div className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Sxtra Spece</div>
 </div>
-<div className="container my-2">
+<div className="container my-2" style={{color:props.mode==='dark'?'white':'black'}}>
     <h2>Your text summary</h2>
     <p>{0.08 * text.split(" ").length} Minutes To read</p>
     <p>{text.split(" ").length} words and {text.length} characters</p>
     <h3>Inner Content</h3>
     <p>{text.split(" ").join(" ")}</p>
     <h4>Preview</h4>
-    <p>{text}</p>
+    <p>{text.length>0?text:"Enter something in the text box above to preview it here"}</p>
 </div>
 </>
 )
